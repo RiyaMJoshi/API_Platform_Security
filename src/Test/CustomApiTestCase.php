aa9905cd9b51e9211c51644d6c92c03768d177d2
Riya Joshi <riyajoshi312@gmail.com>
2022-04-07 16:53:01 +0530

    API Secuirty Upto 'Ch-15: Base Test Class (Error in 401)'

---
+++ src/Test/CustomApiTestCase.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Test;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
+use App\Entity\User;
+
+class CustomApiTestCase extends ApiTestCase
+{
+    protected function createUser(string $email, string $password): User
+    {
+        $container = static::getContainer();
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setUsername(substr($email, 0, strpos($email, '@')));
+        $hashedPassword = $container->get('test.security.password_hasher')
+            ->hashPassword($user, $password);
+        // 'security.password_encoder' doesn't work for Symfony 5, so using 'password_hasher'
+        /*
+        $encoded = $container->get('security.password_encoder')
+            ->encodePassword($user, $password);
+        */
+        $user->setPassword($hashedPassword);
+
+        $em = $container->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $user;
+    }
+
+    protected function logIn(Client $client, string $email, string $password)
+    {
+        $client->request('POST', '/login', [
+            'json' => [
+                'email' => $email,
+                'password' => $password
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(204);
+    }
+
+    protected function createUserAndLogIn(Client $client, string $email, string $password): User
+    {
+        $user = $this->createUser($email, $password);
+        
+        $this->logIn($client, $email, $password);
+
+        return $user;
+    }
+}
+
+?>
--- tests/Functional/CheeseListingResourceTest.php
+++ tests/Functional/CheeseListingResourceTest.php
@@ -6,11 +6,12 @@ use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 use App\Entity\User;
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+//use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Test\CustomApiTestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 
-class CheeseListingResourceTest extends ApiTestCase
+class CheeseListingResourceTest extends CustomApiTestCase
 {
     use ReloadDatabaseTrait;
 
@@ -20,27 +21,30 @@ class CheeseListingResourceTest extends ApiTestCase
         $container = static::getContainer();
       
         $client->request('POST', '/api/cheeses', [
-            'headers' => ['Content-Type' => 'application/json'],
             'json' => [],
         ]);
         $this->assertResponseStatusCodeSame(401);
         
-        $user = new User();
-        $user->setEmail('riyajoshi312@gmail.com');
-        $user->setUsername('riyaJoshi');
-        $user->setPassword('$argon2id$v=19$m=65536,t=6,p=1$AIC3IESQ64NgHfpVQZqviw$1c7M56xyiaQFBjlUBc7T0s53/PzZCjV56lbHnhOUXx8');
-        $em = $container->get('doctrine')->getManager();
-        $em->persist($user);
-        $em->flush();
+        $this->createUserAndLogIn($client, 'riyajoshi312@gmail.com', 'foo');
 
-        $client->request('POST', '/login', [
+        // Try doing the below code for returning 201 status code
+        /*
+        $client->request('POST', '/api/cheeses', [
             'headers' => ['Content-Type' => 'application/json'],
-            'json' => [
-                'email' => 'riyajoshi312@gmail.com',
-                'password' => 'foo'
+            'json' => [ 
+                'owner_id' => 1,
+                'title' => "title1Cheese",
+                'description' => "desc1cheese",
+                'price' => 3000
             ],
         ]);
-        $this->assertResponseStatusCodeSame(204);
+        $this->assertResponseStatusCodeSame(201);
+        */
+
+        $client->request('POST', '/api/cheeses', [
+            'json' => [],
+        ]);
+        $this->assertResponseStatusCodeSame(400);
     }
   
 }
