3f16674f0dd288b3bffb1378e729c87978a02334
Riya Joshi <riyajoshi312@gmail.com>
2022-04-08 14:20:13 +0530

    Upto 'Ch-20: Data Persister- Encoding the PlainPassword'

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -68,6 +68,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @Groups({"user:write"}) 
+     * @SerializedName("password")
     */
     private $plainPassword;
 
